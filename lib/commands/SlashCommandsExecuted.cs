1c832b59728c6a7b7c086013d735623b05fd1e8f
Solar <solar@lunar-dev.com>
2022-03-21 09:15:51 +0000

    Updated SlashCommandHandler
    + Added role case to the cmd switch
    + Added a RoleCmd manager

--- lib/commands/SlashCommandsExecuted.cs
+++ lib/commands/SlashCommandsExecuted.cs
@@ -17,9 +17,35 @@ namespace Bot.Commands
                 case "say":
                     await cmd.RespondAsync(cmd.Data.Options.First().Value.ToString());
                     break;
-                    
+
+                case "role":
+                    await RoleCmd(cmd);
+                    break;
+
                 default:
-                    await cmd.RespondAsync($"Cannot find Slash Command with the name, {cmd.Data.Name}");
+                    await cmd.RespondAsync($"Cannot find Slash Command with the name, {cmd.Data.Name} on the latest version of me.",ephemeral:true);
+                    break;
+            }
+        }
+
+        public async Task RoleCmd(SocketSlashCommand cmd)
+        {
+            switch (cmd.Data.Options.First().Name)
+            {
+                case "add":
+                    var AddUsr = (SocketGuildUser) cmd.Data.Options.First().Options.First().Value;
+                    var AddRole = (SocketRole)cmd.Data.Options.First().Options.Last().Value;
+
+                    await AddUsr.AddRoleAsync(AddRole);
+                    await cmd.RespondAsync($"{AddRole.Mention} has been added to {AddUsr.Mention}");
+                    break;
+                
+                case "remove":
+                    var RemoveUsr = (SocketGuildUser)cmd.Data.Options.First().Options.First().Value;
+                    var RemoveRole = (SocketRole)cmd.Data.Options.First().Options.Last().Value;
+
+                    await RemoveUsr.RemoveRoleAsync(RemoveRole);
+                    await cmd.RespondAsync($"{RemoveRole.Mention} has been remove from {RemoveUsr.Mention}");
                     break;
             }
         }
