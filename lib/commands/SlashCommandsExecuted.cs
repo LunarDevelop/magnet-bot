b360544120dfe844d4a2e52a67d09f489e570063
Solar <solar@lunar-dev.com>
2022-03-26 00:35:12 +0000

    Updated SlashCommands
    I have updated both the Register and Executer
    With a range of Neko Commands from the NekoApi
    
    Also added a check for RolesCmd
    This is to ensure the user has the correct permissions
    This should be abled to be removed when permission V2 comes out.

--- lib/commands/SlashCommandsExecuted.cs
+++ lib/commands/SlashCommandsExecuted.cs
@@ -1,11 +1,13 @@
 using Discord.WebSocket;
 using Discord;
+using NekosSharp;
 
 namespace Bot.Commands
 {
     public class SlashCommandsHandler
     {
         private DiscordSocketClient client;
+        public static NekoClient NekoClient = new NekoClient("Magnet");
         public SlashCommandsHandler(DiscordSocketClient client)
         {
             this.client = client;
@@ -23,6 +25,10 @@ namespace Bot.Commands
                     await RoleCmd(cmd);
                     break;
 
+                case "neko":
+                    await NekoCmd(cmd);
+                    break;
+
                 default:
                     await cmd.RespondAsync($"Cannot find Slash Command with the name, {cmd.Data.Name} on the latest version of me.",ephemeral:true);
                     break;
@@ -33,6 +39,15 @@ namespace Bot.Commands
         {
             SocketRole Role;
             SocketGuildUser Usr;
+
+            SocketTextChannel TxtChannel = (SocketTextChannel) client.GetChannel(cmd.Channel.Id);
+            SocketGuildUser CmdUser = TxtChannel.GetUser(cmd.User.Id);
+
+            if (!CmdUser.GuildPermissions.Has(GuildPermission.ManageRoles))
+            {
+                await cmd.RespondAsync("You do not have permission to do that", ephemeral: true);
+                return;
+            }
 
             switch (cmd.Data.Options.First().Name)
             {
@@ -70,5 +85,93 @@ namespace Bot.Commands
 
             }
         }
+        
+        public async Task NekoCmd(SocketSlashCommand cmd)
+        {
+            Request req;
+            EmbedBuilder embed;
+
+            switch (cmd.Data.Options.First().Name)
+            {
+                case "neko":
+                    req = await NekoClient.Image_v3.Neko();
+                    embed = new EmbedBuilder()
+                        .WithImageUrl(req.ImageUrl);
+
+                    await cmd.RespondAsync(embed: embed.Build());
+                    break;
+
+                case "misc":
+                    switch (cmd.Data.Options.First().Options.First().Name)
+                    {
+                        case "cat":
+                            req = await NekoClient.Misc_v3.Cat();
+                            embed = new EmbedBuilder()
+                                .WithImageUrl(req.ImageUrl);
+
+                            await cmd.RespondAsync(embed: embed.Build());
+                            break;
+                        case "8ball":
+                            req = await NekoClient.Misc_v3.EightBall();
+                            embed = new EmbedBuilder()
+                                .WithImageUrl(req.ImageUrl);
+
+                            await cmd.RespondAsync(embed: embed.Build());
+                            break;
+
+                    }
+                    break;
+
+                case "nsfw":
+                    SocketTextChannel channel = (SocketTextChannel) client.GetChannel(cmd.Channel.Id);
+                    if (!channel.IsNsfw)
+                    {
+                        await cmd.RespondAsync("This is not an NSFW channel, please try there first!", ephemeral:true);
+                        return;
+                    };
+
+                    switch (cmd.Data.Options.First().Options.First().Name)
+                    {
+                        case "pussy":
+                            req = await NekoClient.Nsfw_v3.Pussy();
+                            embed = new EmbedBuilder()
+                                .WithImageUrl(req.ImageUrl);
+
+                            await cmd.RespondAsync(embed: embed.Build());
+                            break;
+                        case "cum":
+                            req = await NekoClient.Nsfw_v3.Cum();
+                            embed = new EmbedBuilder()
+                                .WithImageUrl(req.ImageUrl);
+
+                            await cmd.RespondAsync(embed: embed.Build());
+                            break;
+                        case "bdsm":
+                            req = await NekoClient.Nsfw_v3.Bdsm();
+                            embed = new EmbedBuilder()
+                                .WithImageUrl(req.ImageUrl);
+
+                            await cmd.RespondAsync(embed: embed.Build());
+                            break;
+                        case "gif-spank":
+                            req = await NekoClient.Nsfw_v3.SpankGif();
+                            embed = new EmbedBuilder()
+                                .WithImageUrl(req.ImageUrl);
+
+                            await cmd.RespondAsync(embed: embed.Build());
+                            break;
+                        case "ero-neko":
+                            req = await NekoClient.Nsfw_v3.EroNeko();
+                            embed = new EmbedBuilder()
+                                .WithImageUrl(req.ImageUrl);
+
+                            await cmd.RespondAsync(embed: embed.Build());
+                            break;
+                    }
+                    break;
+            }
+
+        }
     }
+
 }
